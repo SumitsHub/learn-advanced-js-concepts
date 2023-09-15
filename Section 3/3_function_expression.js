b567f8fdeebef4c36b21825850f02372587415b9
Sumit Paul <pauls1317@gmail.com>
2023-09-15 15:44:40 +0530

    Function expression and statement

---
+++ Section 3/3_function_expression.js
@@ -0,0 +1,45 @@
+// Function Object -> Functions are object
+
+function greet() {
+  console.log("Heyy");
+}
+
+greet.language = "English";
+
+console.log(greet.language); // English
+console.log(greet.name); // greet
+
+// Function Statements and Function Expressions
+a = "Hello"; // implicit declaration of variable without var keyword
+console.log(a);
+
+// following lines gives error
+// var val = if (a === 'Hello') {
+//     return 8;
+// }
+
+/** Function statement */
+// using this inside function
+function sayHi() {
+  console.log(this); // Window object\
+}
+// sayHi();
+
+/** function expression */
+// anonymousFunction(); // ERROR -> because it's undefined
+
+var anonymousFunction = function () {
+  console.log("This is an anonymous function");
+};
+
+anonymousFunction();
+console.log(anonymousFunction.name); // anonymousFunction
+
+// passing function as paramter
+function call(callback) {
+  callback(); // Hello
+}
+
+call(function () {
+  console.log("Hello");
+});
