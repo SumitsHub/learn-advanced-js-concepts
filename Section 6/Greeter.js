aec21979b2e2759015febf0a81c8c2073703b304
Sumit Paul <pauls1317@gmail.com>
2023-09-17 22:16:04 +0530

    functions to library

--- Section 6/Greeter.js
+++ Section 6/Greeter.js
@@ -1,1 +1,84 @@
-(function (global, $) {})(window, jQuery);
+(function (global, $) {
+  var Greeter = function (firstName, lastName, language) {
+    return new Greeter.init(firstName, lastName, language);
+  };
+
+  var supportedLanguages = ["en", "es"];
+
+  var greetings = {
+    en: "Hello",
+    es: "Hola",
+  };
+
+  var formalGreetings = {
+    en: "Greetings",
+    es: "Saludos",
+  };
+
+  var logMessages = {
+    en: "Logged in",
+    es: "Inicio session",
+  };
+  Greeter.prototype = {
+    fullName: function () {
+      return `${this.firstName} ${this.lastName}`;
+    },
+
+    validate: function () {
+      if (supportedLanguages.indexOf(this.language) === -1) {
+        throw "Invalid language";
+      }
+    },
+
+    greeting: function () {
+      return greetings[this.language] + " " + this.firstName + "!";
+    },
+
+    formalGreeting: function () {
+      return formalGreetings[this.language] + " " + this.fullName() + "!";
+    },
+
+    greet: function (formal) {
+      var msg;
+
+      if (formal) {
+        msg = this.formalGreeting();
+      } else {
+        msg = this.greeting();
+      }
+
+      if (console) {
+        console.log(msg);
+      }
+
+      return this;
+    },
+
+    log() {
+      if (console) {
+        console.log(logMessages[this.language] + ": " + this.fullName());
+      }
+
+      return this;
+    },
+
+    setLang: function (lang) {
+      this.validate(lang);
+
+      this.language = lang;
+
+      return this;
+    },
+  };
+
+  Greeter.init = function (firstName, lastName, lanuage) {
+    var self = this;
+    self.firstName = firstName || "";
+    self.lastName = lastName || "";
+    self.language = lanuage || "en";
+  };
+
+  Greeter.init.prototype = Greeter.prototype;
+
+  global.Greeter = global.G$ = Greeter;
+})(window, jQuery);
