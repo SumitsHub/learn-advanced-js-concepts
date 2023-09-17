888d5b47fcd1cea513d4381eafeb28801f55ffb6
Sumit Paul <pauls1317@gmail.com>
2023-09-17 18:20:06 +0530

    using class to create object

--- Notes.md
+++ Notes.md
@@ -195,3 +195,7 @@ if (!Object.create) {
   };
 }
 ```
+
+### Syntactic Sugar
+
+A different way to type something that doesn't change how it works under the hood
---
+++ Section 5/5_class.js
@@ -0,0 +1,25 @@
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  greet() {
+    // by default this method is set into prototype
+    return `Hey ${this.firstName} ${this.lastName}`;
+  }
+}
+
+class ChildPerson extends Person {
+  constructor(firstName, lastName) {
+    super(firstName, lastName);
+  }
+}
+
+var john = new Person("John", "Doe");
+
+console.log(john);
+
+var mike = new ChildPerson("Mike", "Smith");
+console.log(mike); // ChildPerson class with Person class prototype
+console.log(mike.greet()); // Hey Mike Smith
