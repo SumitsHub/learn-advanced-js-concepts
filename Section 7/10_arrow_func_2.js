a6d79e2451cbad77706973c5ae0ab447d373b94f
Sumit Paul <pauls1317@gmail.com>
2023-09-26 15:42:10 +0530

    more about arrow functions

---
+++ Section 7/10_arrow_func_2.js
@@ -0,0 +1,55 @@
+/** returning value from arrow functions */
+
+// avoiding return keyword
+let mult = (a, b) => a * b;
+
+console.log(mult(5, 6)); // 30
+
+// avoiding brackets in case of single input
+let sqrt = x => x ** 2;
+console.log(sqrt(4));
+
+// typeof arrow functions
+console.log(typeof sqrt); // functions
+
+// Readability ->
+
+let longExpression = x => y => z => z ? y : x;
+console.log(longExpression(5)(6)(false)); // 5
+
+// Limitations ->
+
+let obj = {
+  firstName: "Tony",
+  name: "Object",
+  printName() {
+    console.log(this.name); // Object -> refers to this objects name property
+  },
+  greet() {
+    console.log(this.firstName); // Tony
+  },
+  greet2: () => {
+    console.log(this); // refers to Window object
+    console.log(this.name); // '' -> window.name
+    console.log(this.firstName); // undefined
+  },
+};
+
+obj.printName();
+console.log(obj.name);
+obj.greet(); // Tony
+obj.greet2();
+
+// Imp -> do not use arrow functions as objects instance methods, it will lead to waste of memory
+
+class Person {
+  constructor(fname) {
+    this.firstName = fname;
+    this.greet = () => {
+      console.log(this.firstName); // here arrow function will refer to this keyword present inside the class constructor
+    };
+  }
+}
+
+let p = new Person("Jony");
+p.greet(); // Jony
